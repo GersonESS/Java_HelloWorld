3433ddeee7b8de41f3575d09491d7c8c690f15ca
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-03-25 21:54:16 -0300

    a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpClient;
+import java.net.URI;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Olá, World!");
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_x3pev8lm";
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest requisicao = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());
+        String json = resposta.body();
+        System.out.println(json);
+
     }
 }
