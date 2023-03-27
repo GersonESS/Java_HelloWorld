5edcba2c25906ba7958b442495341f6115d4ca1a
Gerson Santos <gersones@outlook.com>
2023-03-27 13:32:46 -0300

    a

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ import java.net.URI;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, World!");
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_x3pev8lm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         HttpClient cliente = HttpClient.newHttpClient();
         HttpRequest requisicao = HttpRequest.newBuilder(URI.create(url)).GET().build();
         HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());
