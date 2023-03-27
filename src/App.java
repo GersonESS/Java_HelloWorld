d3326491b021496a6236d72d904535fccbba78ec
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-03-27 15:46:58 -0300

    a

--- src/App.java
+++ src/App.java
@@ -1,18 +1,26 @@
+import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.net.http.HttpClient;
-import java.net.URI;
+import java.util.Map;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, World!");
+        // System.out.println("Olá, World!");
+        // Fazrt conexao http
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         HttpClient cliente = HttpClient.newHttpClient();
         HttpRequest requisicao = HttpRequest.newBuilder(URI.create(url)).GET().build();
         HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());
-        String json = resposta.body();
-        System.out.println(json);
+        String body = resposta.body();
+        System.out.println(body);
+        // extrair os dados
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFimes = parser.parse(body);
+        //
 
     }
 }
